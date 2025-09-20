7bab892800af5369a407618ea14705e938d72409
Dawood Idrees <dawood.idrees@ecomexperts.io>
2025-09-20 06:09:47 +0500

    Update index.html notification text, adjust swiper slides per view in script.js, and enhance responsive styles in style.css for improved layout and usability.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const scrolling_images = document.querySelector("#scrolling__images");
 if (scrolling_images) {
   var swiper = new Swiper("#scrolling__images .mySwiper", {
     spaceBetween: 10,
-    slidesPerView: 4,
+    slidesPerView: 3,
     speed: 5000,
     allowTouchMove: false,
     simulateTouch: false,
